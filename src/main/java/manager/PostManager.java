40c9f419d10509f95d9b0672f2b0812d543ace39
Tanya <tanya.leukhina@mail.ru>
2020-12-11 22:56:45 +0300

    PostManager created

---
+++ src/main/java/manager/PostManager.java
@@ -0,0 +1,13 @@
+package manager;
+
+public class PostManager {
+    private domain.Post[] posts;
+
+    public domain.Post[] search(int ownerId, String domain, String query, boolean ownersOnly, int count, int offset){
+        return null;
+    }
+
+    public void delete(int ownerId, int postId){
+
+    }
+}
